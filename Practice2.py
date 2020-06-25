3154f1a644fd7f861cd34c9a9253cead63e460b5
antonialam <anla2291@colorado.edu>
2020-06-25 01:48:58 -0600

    Edited Practice2.py

--- Practice2.py
+++ Practice2.py
@@ -13,6 +13,14 @@ def initialize():
                         '--xvg',
                         nargs='+',
                         help='The name of the file being analyzed.')
+    parser.add_argument('-n',
+                        '--nvars',
+                        help='The number of dependent variables. This flag allows \n'
+                             'for different columns to be plotted along the \n'
+                             'y_axis.')
+    parser.add_argument('-z',
+                        '--size',
+                        help='This specifies the dimensions of the plot.')
     parser.add_argument('-t',
                         '--title',
                         help='The title of the plot.')
@@ -22,34 +30,46 @@ def initialize():
     parser.add_argument('-y',
                         '--ylabel',
                         help='The label and units of the y-axis.')
+    parser.add_argument('-l',
+                        '--legend',
+                        nargs='+',
+                        help='This allows the user to specify the legend of the plot.')
+    parser.add_argument('-s',
+                        '--save',
+                        help='This specifies what the plot should be saved as.')
 
     args_parse = parser.parse_args()
 
     return args_parse
 
 
-args = initialize()
+if __name__ == "__main__":
+    args = initialize()
 
-def analyze(*args):
-# Reading the file
-    for file in args:
-        f = open(file,'r')
+    # Reading the file
+    for file in args.xvg:
+        f = open(file, 'r')
         lines = f.readlines()
         f.close()
 
-# Extracting data from the file
-        x, y = [], []
+    # Extracting data from the file
+        x = []
+        y = []
+        for i in range(int(args.nvars)):
+            y.append([])
+
         for line in lines:
             if line[0] != '#' and line[0] != '@':
                 x.append(float(line.split()[0]))
-                y.append(float(line.split()[1]))
+                for i in range(int(args.nvars)):
+                    y[i].append(float(line.split()[i + 1]))
 
-# Executing the RMSF equation
+    # Executing the RMSF equation
         Q = np.mean(y)  # <Q>
-        Q2 = np.mean(np.power(y, 2))  # <Q^2>
+        Q2 = np.mean(np.power(y[0], 2))  # <Q^2>
         RMSF = ((Q2 - (Q ** 2)) ** 0.5) / Q
 
-# Graphing the plot
+    # Customizing the font of the plot
         rc('font', **{
             'family': 'sans-serif',
             'sans-serif': ['DejaVu Sans'],
@@ -57,31 +77,33 @@ def analyze(*args):
         rc('mathtext', **{'default': 'regular'})
         plt.rc('font', family='serif')
 
-        plt.plot(x, y, linewidth=0.5)
-        plt.title("Volume as a function of time")
-        plt.xlabel("Time (ps)")
-        plt.ylabel("Volume ($nm^3$)")
+    # Graphing the plot
+        for i in range(int(args.nvars)):
+            plt.plot(x, y[i], linewidth=0.5)
+        plt.title(args.title)
+        plt.xlabel(args.xlabel)
+        plt.ylabel(args.ylabel)
         plt.grid()
-        # plt.savefig('Volume1.png', dpi=600)
-        plt.show()
+        plt.legend(args.legend)
+        plt.savefig(args.save, dpi=600)
+        # plt.show()
 
-# Important values
-        average_volume = round(Q, 3)
-        RMSF_rounded = round(RMSF, 3)
-        max_volume = round(max(y), 3)
-        min_volume = round(min(y), 3)
-        time_of_max_volume = round(x[y.index(max(y))]/1000, 4)
-        time_of_min_volume = round(x[y.index(min(y))]/1000, 4)
-        time_of_value_closest_to_Q = round(x[y.index(min(y, key=lambda x: abs(x-Q)))]/1000, 3)
-        value_closest_to_Q = round(min(y, key=lambda x: abs(x-Q)), 6)
+    # Important values
+        average_volume = Q
+        RMSF_rounded = RMSF
+        max_volume = max(y[0])
+        min_volume = min(y[0])
+        time_of_max_volume = x[y[0].index(max(y[0]))]/1000
+        time_of_min_volume = x[y[0].index(min(y[0]))]/1000
+        time_of_value_closest_to_Q = x[y[0].index(min(y[0], key=lambda x: abs(x-Q)))]/1000
+        value_closest_to_Q = min(y[0], key=lambda x: abs(x-Q))
 
-# Printing out statistics
-        print("Data analysis of the file: volume.xvg")
+    # Printing out statistics
+        print("Data analysis of the file: " + "".join((args.xvg)))
         print("=====================================")
         print("Analyzing the file ...")
         print("Plotting and saving figure ...")
-        print(f"The average of volume (nm^3): {average_volume} (RMSF: {RMSF_rounded}, max: {max_volume}, min: {min_volume})")
-        print(f"The maximum occurs at {time_of_max_volume} ns while the minimum occurs at {time_of_min_volume} ns.")
-        print(f"The configuration at {time_of_value_closest_to_Q} ns has the volume {value_closest_to_Q} nm^3 that is closest to the average volume.")
-
-analyze('volume.xvg','volume2.xvg')
+        print(f'The average of volume (nm^3):{average_volume: .3f} (RMSF:{RMSF_rounded: .3f}, max:{max_volume: .3f}, min:{min_volume: .3f})')
+        print(f'The maximum occurs at{time_of_max_volume: .4f} ns while the minimum occurs at{time_of_min_volume: .4f} ns.')
+        print(f'The configuration at{time_of_value_closest_to_Q: .3f} ns has the volume{value_closest_to_Q: .6f} nm^3 that is closest to the average volume.')
+        
