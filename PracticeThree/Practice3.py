0067341ddcabbc07cf000dc9420a4a754630af1d
antonialam <anla2291@colorado.edu>
2020-06-30 02:05:13 -0600

    Added PracticeThree/Practice3.py

---
+++ PracticeThree/Practice3.py
@@ -0,0 +1,49 @@
+import itertools
+import numpy as np
+
+
+
+def initialize_particles(p, l):
+    """
+    Parameters:
+        p (int): The number of particles in the simulation box.
+        l (float): The length of the side of the simulation box.
+    Returns:
+        The random 3-dimensional coordinates of each particle.
+    """
+    return np.random.uniform(low=-l/2, high=l/2, size=(p, 3))
+# print(initialize_particles(5,1))
+
+
+def calc_distance(a, b, l):
+    """
+       Parameters:
+           a (list): The 3-dimensional coordinates of a particle.
+           b (list): The 3-dimensional coordinates of another particle.
+           l (float): The length of the side of the simulation box.
+       Returns:
+           r (float): The distance between two particles with the consideration of periodic boundary conditions (PBCs).
+       """
+    x = (a[0] - b[0]) ** 2
+    y = (a[1] - b[1]) ** 2
+    z = (a[2] - b[2]) ** 2
+    distance = (x + y + z) ** 0.5
+    if distance > 0.5 * l:
+        for n in itertools.count(start=1):
+            if distance - (n * l) < 0.5 * l:
+                break
+    return abs(distance - (n * l))
+# print(calc_distance([1,2,5],[1,2,100],7.0))
+
+
+def lennard_jones(a, b, l):
+    """
+       Returns:
+           potential (float): Uses the calc_distance function to return the Lennard-Jones potential between two particles.
+       """
+    r = calc_distance(a, b, l)
+    potential = 4 * ((1 / (r**12)) - (1 / (r**6)))
+    return potential
+
+
+print(lennard_jones([1, 2, 5], [1, 2, 100], 7.0))
