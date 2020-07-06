6d95459707cd7b609bec3a84b8fb757c080c7685
antonialam <anla2291@colorado.edu>
2020-07-06 13:16:07 -0600

    Modified Practice3.py

--- PracticeThree/Practice3.py
+++ PracticeThree/Practice3.py
@@ -1,72 +1,119 @@
-import itertools
+import time
 from itertools import combinations
 import numpy as np
+from matplotlib import rc
+import matplotlib.pyplot as plt
 
 
-
-def initialize_particles(p, l):
+def initialize_particles(particles, box_length):
     """
     Parameters:
-        p (int): The number of particles in the simulation box.
-        l (float): The length of the side of the simulation box.
+        particles (int): The number of particles in the simulation box.
+        box_length (float): The length of the side of the simulation box.
     Returns:
         The random 3-dimensional coordinates of each particle.
     """
-    return np.random.uniform(low=-l/2, high=l/2, size=(p, 3))
+    return np.random.uniform(low=-box_length/2, high=box_length/2, size=(particles, 3))
 # print(initialize_particles(5,1))
 
 
-def calc_distance(a, b, l):
+def calc_distance(coord_a, coord_b, box_length):
     """
        Parameters:
-           a (list): The 3-dimensional coordinates of a particle.
-           b (list): The 3-dimensional coordinates of another particle.
-           l (float): The length of the side of the simulation box.
+           coord_a (list): The 3-dimensional coordinates of a particle.
+           coord_b (list): The 3-dimensional coordinates of another particle.
+           box_length (float): The length of the side of the simulation box.
        Returns:
-           r (float): The distance between two particles with the consideration of periodic boundary conditions (PBCs).
+           distance (float): The distance between two particles with the consideration of periodic boundary conditions (PBCs).
     """
-    x = (a[0] - b[0]) ** 2
-    y = (a[1] - b[1]) ** 2
-    z = (a[2] - b[2]) ** 2
-    distance = (x + y + z) ** 0.5
-    if distance > 0.5 * l:
-        global n
-        for n in itertools.count(start=1):
-            if distance - (n * l) < 0.5 * l:
-                break
-    return abs(distance - (n * l))
+    r = np.array(coord_a) - np.array(coord_b)
+    r = r - box_length * np.round(r / box_length)
+    distance = np.linalg.norm(r)
+    return distance
 # print(calc_distance([1,2,5],[1,2,100],7.0))
 
 
-def lennard_jones(a, b, l):
+def lennard_jones(coord_a, coord_b, box_length, epsilon=1, sigma=1):
     """
+        Parameters:
+            coord_a (list): The 3-dimensional coordinates of a particle.
+            coord_b (list): The 3-dimensional coordinates of another particle.
+            box_length (float): The length of the side of the simulation box.
        Returns:
-           potential (float): Uses the calc_distance function to return the Lennard-Jones potential between two particles.
+            potential (float): Uses the calc_distance function to return the Lennard-Jones potential between two particles.
     """
-    r = calc_distance(a, b, l)
-    potential = 4 * ((1 / (r**12)) - (1 / (r**6)))
+    distance = calc_distance(coord_a, coord_b, box_length)
+    potential = 4 * epsilon * \
+        ((sigma / (distance**12)) - (sigma / (distance**6)))
     return potential
-#print(lennard_jones([1, 2, 5], [1, 2, 100], 7.0))
+# print(lennard_jones([1, 2, 5], [1, 2, 100], 7.0))
 
 
-def calc_sys_potential(p, coordinates, l):
+def calc_sys_potential(particles,coordinates, box_length):
+    """
+       Parameters:
+           particles (int): The number of particles in the simulation box.
+           coordinates (array): The coordinates of all of the particles in the simulation box.
+           box_length (float): The length of the side of the simulation box.
+       Returns:
+           The random 3-dimensional coordinates of each particle.
+       """
     total = 0
-    for i in combinations(list(range(1, p + 1)), 2):
-        total += lennard_jones(coordinates[i[0] - 1], coordinates[i[1] - 1], l)
+    for i in combinations(list(range(1, particles + 1)), 2):
+        total += lennard_jones(coordinates[i[0] - 1], coordinates[i[1] - 1], box_length)
     return total
-
-x = ([[0.280658  , 0.5280338, 0 ],
-       [0.89481114, 0.75999593, 0],
-       [0.80558062, 0.52409808, 0],
-       [0.07831073, 0.74807289, 0],
-       [0.07833195, 0.08513294, 0]])
-
-print(calc_sys_potential(5,x,1))
+x = ([[0.280658, 0.5280338, 0],
+      [0.89481114, 0.75999593, 0],
+      [0.80558062, 0.52409808, 0],
+      [0.07831073, 0.74807289, 0],
+      [0.07833195, 0.08513294, 0]])
+# print(calc_sys_potential(5,x,1))
 
 
+# Graphing the time elapsed calculating energy as a function of the number of particles
+x = []
+y = []
+for num in range(2, 501):
+    random_coordinates = initialize_particles(num, 5)
+    t1 = time.time()
+    total_energy = calc_sys_potential(num, random_coordinates, 5)
+    t2 = time. time()
+    elapsed_time = t2 - t1
+    x.append(num)
+    y.append(elapsed_time)
+
+rc('font', **{
+    'family': 'sans-serif',
+    'sans-serif': ['DejaVu Sans'],
+    'size': 10})
+rc('mathtext', **{'default': 'regular'})
+plt.rc('font', family='serif')
+
+plt.title("Time Elapsed Calculating Energy as a Function of the Number of Particles")
+plt.xlabel("Number of Particles")
+plt.ylabel("Time Elapsed Calculating Energy (sec)")
+plt.grid()
+plt.plot(x, y, linewidth=0.8)
+plt.show()
 
 
+# Graphing histogram of total energy frequencies
+y = []
+for num in range(1, 1001):
+    random_coordinates = initialize_particles(100, 5)
+    total_energy = calc_sys_potential(100, random_coordinates, 5)
+    y.append(total_energy)
 
+rc('font', **{
+    'family': 'sans-serif',
+    'sans-serif': ['DejaVu Sans'],
+    'size': 10})
+rc('mathtext', **{'default': 'regular'})
+plt.rc('font', family='serif')
 
-
-
+plt.title("Distribution of Total Energy")
+plt.xlabel("Total Energy")
+plt.ylabel("Number of Trials")
+plt.grid()
+plt.hist(y, bins=1000)
+plt.show()
