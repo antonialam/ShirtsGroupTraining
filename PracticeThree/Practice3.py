1e8960713dc7b650b0e0e34c3a3d702a95c10574
antonialam <anla2291@colorado.edu>
2020-07-14 22:24:35 -0600

    Finalized changes to Practice3.py

--- PracticeThree/Practice3.py
+++ PracticeThree/Practice3.py
@@ -3,7 +3,6 @@ from itertools import combinations
 import numpy as np
 from matplotlib import rc
 import matplotlib.pyplot as plt
-import statistics
 
 
 def initialize_particles(particles, box_length):
@@ -14,8 +13,7 @@ def initialize_particles(particles, box_length):
     Returns:
         The random 3-dimensional coordinates of each particle.
     """
-    return np.random.uniform(low=-box_length/2, high=box_length/2, size=(particles, 3))
-# print(initialize_particles(5,1))
+    return (np.random.rand(particles, 3) - 0.5) * box_length
 
 
 def calc_distance(coord_a, coord_b, box_length):
@@ -31,7 +29,6 @@ def calc_distance(coord_a, coord_b, box_length):
     r = r - box_length * np.round(r / box_length)
     distance = np.linalg.norm(r)
     return distance
-# print(calc_distance([1,2,5],[1,2,100],7.0))
 
 
 def lennard_jones(coord_a, coord_b, box_length, epsilon=1, sigma=1):
@@ -47,7 +44,6 @@ def lennard_jones(coord_a, coord_b, box_length, epsilon=1, sigma=1):
     potential = 4 * epsilon * \
         ((sigma / (distance**12)) - (sigma / (distance**6)))
     return potential
-# print(lennard_jones([1, 2, 5], [1, 2, 100], 7.0))
 
 
 def calc_sys_potential(particles, coordinates, box_length):
@@ -65,75 +61,62 @@ def calc_sys_potential(particles, coordinates, box_length):
     return total
 
 
-sample = ([[0.280658, 0.5280338, 0],
-           [0.89481114, 0.75999593, 0],
-           [0.80558062, 0.52409808, 0],
-           [0.07831073, 0.74807289, 0],
-           [0.07833195, 0.08513294, 0]])
-# print(calc_sys_potential(5,sample,1))
+if __name__ == "__main__":
+
+    # Graphing the time elapsed calculating energy as a function of the number of particles
+    particle_num = []
+    time_y = []
+    for num in range(2, 101):
+        random_coordinates = initialize_particles(num, 5)
+        t1 = time.time()
+        total_energy = calc_sys_potential(num, random_coordinates, 5)
+        t2 = time. time()
+        elapsed_time = t2 - t1
+        particle_num.append(num)
+        time_y.append(elapsed_time)
+
+    rc('font', **{
+        'family': 'sans-serif',
+        'sans-serif': ['DejaVu Sans'],
+        'size': 10})
+    rc('mathtext', **{'default': 'regular'})
+    plt.rc('font', family='serif')
+
+    plt.title("Time Elapsed Calculating Energy as a Function of the Number of Particles")
+    plt.xlabel("Number of Particles")
+    plt.ylabel("Time Elapsed Calculating Energy (sec)")
+    plt.grid()
+    plt.plot(particle_num, time_y, linewidth=0.8)
+    plt.show()
 
 
-# Graphing the time elapsed calculating energy as a function of the number of particles
-particle_num = []
-time_y = []
-for num in range(2, 101):
-    random_coordinates = initialize_particles(num, 5)
-    t1 = time.time()
-    total_energy = calc_sys_potential(num, random_coordinates, 5)
-    t2 = time. time()
-    elapsed_time = t2 - t1
-    particle_num.append(num)
-    time_y.append(elapsed_time)
+    # Graphing histogram of total energy frequencies
+    y = []
+    for num in range(1, 1001):
+        random_coordinates = initialize_particles(100, 5)
+        total_energy = calc_sys_potential(100, random_coordinates, 5)
+        y.append(total_energy)
 
-rc('font', **{
-    'family': 'sans-serif',
-    'sans-serif': ['DejaVu Sans'],
-    'size': 10})
-rc('mathtext', **{'default': 'regular'})
-plt.rc('font', family='serif')
+    plt.title("Distribution of Total Energy")
+    plt.xlabel("Total Energy (J)")
+    plt.ylabel("Number of Trials")
+    plt.grid()
+    plt.hist(y, bins=1000)
+    plt.show()
 
-plt.title("Time Elapsed Calculating Energy as a Function of the Number of Particles")
-plt.xlabel("Number of Particles")
-plt.ylabel("Time Elapsed Calculating Energy (sec)")
-plt.grid()
-plt.plot(particle_num, time_y, linewidth=0.8)
-plt.show()
+    # Important variables
+    average = np.mean(y)
+    med = (y[499] + y[500]) / 2
+    max = max(y)
+    min = min(y)
+    std = np.std(y)
 
-
-# Graphing histogram of total energy frequencies
-y = []
-for num in range(1, 1001):
-    random_coordinates = initialize_particles(100, 5)
-    total_energy = calc_sys_potential(100, random_coordinates, 5)
-    y.append(total_energy)
-
-rc('font', **{
-    'family': 'sans-serif',
-    'sans-serif': ['DejaVu Sans'],
-    'size': 10})
-rc('mathtext', **{'default': 'regular'})
-plt.rc('font', family='serif')
-
-plt.title("Distribution of Total Energy")
-plt.xlabel("Total Energy (J)")
-plt.ylabel("Number of Trials")
-plt.grid()
-plt.hist(y, bins=1000)
-plt.show()
-
-# Important variables
-average = np.mean(y)
-med = (y[499] + y[500]) / 2
-max = max(y)
-min = min(y)
-std = statistics.stdev(y)
-
-# Print statistics
-result_str = "Data analysis of the file"
-print(result_str)
-print("=" * len(result_str))
-print("Analyzing the file ...")
-print("Plotting and saving figure ...")
-print(f"Average: {average:.3e} J\nMedian: {med:.3e} J")
-print(f"Max: {max:.3e} J    Min: {min:.3e} J")
-print(f"Std Dev: {std:.3e} J")
+    # Print statistics
+    result_str = "Data analysis of the file"
+    print(result_str)
+    print("=" * len(result_str))
+    print("Analyzing the file ...")
+    print("Plotting and saving figure ...")
+    print(f"Average: {average:.3e} J\nMedian: {med:.3e} J")
+    print(f"Max: {max:.3e} J    Min: {min:.3e} J")
+    print(f"Std Dev: {std:.3e} J")
